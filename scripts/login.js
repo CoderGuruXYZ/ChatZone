9c178119a1c43c4c9b0b5465ed70e126e4bfbe5a
CoderGuruXYZ <72657147+CoderGuruXYZ@users.noreply.github.com>
2022-02-21 10:51:54 +0000

    :(

--- scripts/login.js
+++ scripts/login.js
@@ -1,6 +1,10 @@
 const queryString = window.location.search;
 
 if (queryString.indexOf("?") > -1) {
+    gapi.load('auth2', function() {
+        gapi.auth2.init();
+    });
+
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         localStorage.removeItem("entity");
