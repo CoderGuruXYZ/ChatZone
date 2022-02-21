f5f888b1e1544b0842a853790838560a0bd3178a
CoderGuruXYZ <72657147+CoderGuruXYZ@users.noreply.github.com>
2022-02-21 10:16:49 +0000

    quick comment

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -3,9 +3,9 @@ if(localStorage.loggedIn == null) {
 }
 
 if(window.location.href.includes("login.html")) {
-    if(JSON.parse(localStorage.loggedIn)) {
-        window.location.href = "home.html";
-    }
+    // if(JSON.parse(localStorage.loggedIn)) {
+    //     window.location.href = "home.html";
+    // }
 } else if(window.location.href.includes("home.html")) {
     if(!JSON.parse(localStorage.loggedIn)) {
         window.location.href = "login.html";
