a08a66f8d12dc8d7a2f7356620dcd31637fa6d8c
CoderGuruXYZ <72657147+CoderGuruXYZ@users.noreply.github.com>
2022-02-21 10:09:21 +0000

    new signout func?

--- scripts/signout.js
+++ scripts/signout.js
@@ -1,8 +1,10 @@
 function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        console.log('User Signed Out.');
-    });
+    // var auth2 = gapi.auth2.getAuthInstance();
+    // auth2.signOut().then(function () {
+    //     console.log('User Signed Out.');
+    // });
+
+    GoogleAuth.signOut();
 };
 
 document.getElementById("signoutBtn").addEventListener("click", function() {
