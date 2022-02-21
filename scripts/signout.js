47aad10ebb24824f13204a48103a98d3e464e5ae
CoderGuruXYZ <72657147+CoderGuruXYZ@users.noreply.github.com>
2022-02-21 10:14:20 +0000

    ok

--- scripts/signout.js
+++ scripts/signout.js
@@ -1,10 +1,8 @@
 function signOut() {
-    // var auth2 = gapi.auth2.getAuthInstance();
-    // auth2.signOut().then(function () {
-    //     console.log('User Signed Out.');
-    // });
-
-    GoogleAuth.signOut();
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User Signed Out.');
+    });
 };
 
 document.getElementById("signoutBtn").addEventListener("click", function() {
